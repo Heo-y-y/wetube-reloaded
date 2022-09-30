41dec4aaae959ca7bc994ddab0484cf102659fe0
Heoyungyoung <112863029+Heoyungyoung@users.noreply.github.com>
2022-09-30 13:34:56 +0900

    wetube1

---
+++ src/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.DB_URL);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handelError = () => console.log("❌ DB Error", error);
+
+db.on("error", handelError);
+db.once("open", handleOpen);
---
+++ src/routers/userRouter.js
@@ -0,0 +1,36 @@
+import express from "express";
+import {
+  getEdit,
+  postEdit,
+  logout,
+  see,
+  startGithubLogin,
+  finishGithubLogin,
+  getChangePasssword,
+  postChangePassword,
+} from "../controllers/userController";
+import {
+  protectorMiddleware,
+  publicOnlyMiddleware,
+  avatarUpload,
+} from "../middlewares";
+
+const userRouter = express.Router();
+
+userRouter.get("/logout", protectorMiddleware, logout);
+userRouter
+  .route("/edit")
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(avatarUpload.single("avatar"), postEdit);
+userRouter
+  .route("/change-password")
+  .all(protectorMiddleware)
+  .get(getChangePasssword)
+  .post(postChangePassword);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
+
+userRouter.get("/:id", see);
+
+export default userRouter;
