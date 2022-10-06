5d8da389a58b9099a4103575a762068f8a572eb0
Heoyungyoung <112863029+Heoyungyoung@users.noreply.github.com>
2022-10-06 12:04:18 +0900

    s3 uploads

--- src/server.js
+++ src/server.js
@@ -12,6 +12,14 @@ const app = express();
 const loggerMiddleware = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+/*app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});*/
 app.use((req, res, next) => {
   res.header("Cross-Origin-Embedder-Policy", "require-corp");
   res.header("Cross-Origin-Opener-Policy", "same-origin");
